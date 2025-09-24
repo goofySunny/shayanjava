2f294ad9715e8c203bd597a4eea07be9faf8b618
bigali <Alinajafian580@gmail.com>
2025-09-24 18:00:07 +0330

    Project specialization setup and house keeping

--- src/main/java/ir/najaftech/ShayanApplication.java
+++ src/main/java/ir/najaftech/ShayanApplication.java
@@ -1,12 +1,12 @@
-package study.jsprendering;
+package ir.najaftech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimpleWebAppTomcatApplication {
+public class ShayanApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimpleWebAppTomcatApplication.class, args);
+		SpringApplication.run(ShayanApplication.class, args);
 	}
 }
