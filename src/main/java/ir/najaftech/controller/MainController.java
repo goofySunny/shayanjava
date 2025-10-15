4f7d4a0e2d8973d3733a607e36547ae617d6953d
Ali <Alinajafian580@gmail.com>
2025-10-15 12:04:11 +0330

    fix: gallery method works correctly now

--- src/main/java/ir/najaftech/controller/MainController.java
+++ src/main/java/ir/najaftech/controller/MainController.java
@@ -65,7 +65,7 @@ public class MainController {
     @GetMapping("/gallery/images/{id}")
     @ResponseBody
     public ResponseEntity<Object> getGalleryImage(@PathVariable Long id) {
-        ShowcaseItem item = showcaseService.getShowCaseItemById(id);
+        ShowcaseItem item = galleryService.getGalleryItemById(id);
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.IMAGE_PNG);
