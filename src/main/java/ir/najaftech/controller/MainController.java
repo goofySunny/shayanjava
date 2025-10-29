18e7b71c20f6fc5b35fcfa5bd9d5feb3e7e046f6
Sun <alinajafian580@gmail.com>
2025-10-29 17:39:43 +0330

    Progression

--- src/main/java/ir/najaftech/controller/MainController.java
+++ src/main/java/ir/najaftech/controller/MainController.java
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -38,6 +40,12 @@ public class MainController {
 
         return "index";
     }
+
+    @GetMapping("/admin")
+    public String getMethodName() {
+        return "admin";
+    }
+    
 
     @GetMapping("/about")
     public String about() {
