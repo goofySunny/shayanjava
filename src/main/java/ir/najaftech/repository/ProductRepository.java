78e781e6d93d83da961948bccfad148c07e01bd9
sun <Alinajafian580@gmail.com>
2025-11-18 16:00:47 +0330

    Important method added for products

--- src/main/java/ir/najaftech/repository/ProductRepository.java
+++ src/main/java/ir/najaftech/repository/ProductRepository.java
@@ -1,9 +1,13 @@
 package ir.najaftech.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ir.najaftech.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findByCategoryId(Long categoryId);
     
 }
