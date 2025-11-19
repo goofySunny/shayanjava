89e796a36cd5d16baad640215b799b5f62b98304
sun <Alinajafian580@gmail.com>
2025-11-19 16:03:24 +0330

    feat: seeding of the database for categories

---
+++ src/main/resources/db/migration/V4__categories_seed.sql
@@ -0,0 +1,12 @@
+INSERT INTO public.categories (cat_name, image_name) VALUES
+('الکترونیکی', 'electronics.jpg');
+INSERT INTO public.categories (cat_name, image_name) VALUES
+('آهنی', 'iron.jpg');
+INSERT INTO public.categories (cat_name, image_name) VALUES
+('پلاستیکی', 'plastics.jpg');
+INSERT INTO public.categories (cat_name, image_name) VALUES
+('آلیاژ', 'alloy.jpg');
+INSERT INTO public.categories (cat_name, image_name) VALUES
+('بتن', 'concrete.jpg');
+INSERT INTO public.categories (cat_name, image_name) VALUES
+('چوبی', 'wooden.jpg');
