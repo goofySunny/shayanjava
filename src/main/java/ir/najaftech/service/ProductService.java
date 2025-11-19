006b48f1b6240aae957be6c2e6b5329034ceb01e
sun <Alinajafian580@gmail.com>
2025-11-19 15:45:23 +0330

    feat[products]: Produt Service and its implementation added

---
+++ src/main/java/ir/najaftech/service/ProductService.java
@@ -0,0 +1,22 @@
+package ir.najaftech.service;
+
+import java.util.List;
+
+import ir.najaftech.dto.request.ProductRequest;
+import ir.najaftech.dto.response.ProductResponse;
+import ir.najaftech.model.Product;
+
+public interface ProductService {
+
+    ProductResponse getProductById(Long id) throws Exception;
+
+    List<ProductResponse> getAllProducts();
+
+    List<ProductResponse> getProductsByCategoryId(Long categoryId);
+
+    ProductResponse createProduct(ProductRequest product) throws Exception;
+
+    void deleteProduct(Long id) throws Exception;
+
+    ProductResponse updateProduct(Long id, ProductRequest productRequest) throws Exception;
+}
