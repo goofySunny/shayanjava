1dc29b2f930e6ae7eea25274f79acf5f428d1bd6
sun <Alinajafian580@gmail.com>
2025-11-19 17:07:56 +0330

    fix:added Ids to the seeds

--- src/main/resources/db/migration/V4__categories_seed.sql
+++ src/main/resources/db/migration/V4__categories_seed.sql
@@ -1,12 +1,12 @@
-INSERT INTO public.categories (cat_name, image_name) VALUES
-('الکترونیکی', 'electronics.jpg');
-INSERT INTO public.categories (cat_name, image_name) VALUES
-('آهنی', 'iron.jpg');
-INSERT INTO public.categories (cat_name, image_name) VALUES
-('پلاستیکی', 'plastics.jpg');
-INSERT INTO public.categories (cat_name, image_name) VALUES
-('آلیاژ', 'alloy.jpg');
-INSERT INTO public.categories (cat_name, image_name) VALUES
-('بتن', 'concrete.jpg');
-INSERT INTO public.categories (cat_name, image_name) VALUES
-('چوبی', 'wood.jpg');
+INSERT INTO public.categories (id, cat_name, image_name) VALUES
+(1, 'الکترونیکی', 'electronics.jpg');
+INSERT INTO public.categories (id, cat_name, image_name) VALUES
+(2, 'آهنی', 'iron.jpg');
+INSERT INTO public.categories (id, cat_name, image_name) VALUES
+(3, 'پلاستیکی', 'plastics.jpg');
+INSERT INTO public.categories (id, cat_name, image_name) VALUES
+(4, 'آلیاژ', 'alloy.jpg');
+INSERT INTO public.categories (id, cat_name, image_name) VALUES
+(5, 'بتن', 'concrete.jpg');
+INSERT INTO public.categories (id, cat_name, image_name) VALUES
+(6, 'چوبی', 'wood.jpg');
