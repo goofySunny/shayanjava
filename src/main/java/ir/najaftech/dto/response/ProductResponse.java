26f8ab1579a3b5e0b59c9af222179e3772deae88
sun <Alinajafian580@gmail.com>
2025-11-19 15:46:06 +0330

    fix: Correction of data structure

--- src/main/java/ir/najaftech/dto/response/ProductResponse.java
+++ src/main/java/ir/najaftech/dto/response/ProductResponse.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ProductResponse {
 
+    private long id;
     private String name;
     private String description;
     private Double price;
